881469d468894921eb1269e249b786fbf52147df
Ftbots <fndmydvc0077@gmail.com>
2025-01-26 03:36:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ API_ID = 26042863  # Your Telegram API ID
 API_HASH = "d4fabc00b0345cd3f0ccdc0c9b750f6e"  # Your Telegram API Hash
 BOT_TOKEN = "7684406232:AAHOKnaKpHJQyChH1YmlsmzS86ZcU1vQByE"  # Your Telegram Bot Token
 
-# Mega Account Credentials
+
 MEGA_EMAIL = "logkithog442@fanclub.pm"
 MEGA_PASSWORD = "Hehe@2580"
 
 LOG_CHANNEL_ID = -1002320532990
+
+ADMIN_USER_IDS = [123456789, 987654321]
 
 
 
