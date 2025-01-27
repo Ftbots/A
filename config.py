f468e2f2c7659674dc5df1dca08d90a9a863c2de
Ftbots <fndmydvc0077@gmail.com>
2025-01-28 01:56:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,10 @@
-API_ID = 26042863  # Your Telegram API ID
-API_HASH = "d4fabc00b0345cd3f0ccdc0c9b750f6e"  # Your Telegram API Hash
-BOT_TOKEN = "7684406232:AAHOKnaKpHJQyChH1YmlsmzS86ZcU1vQByE"  # Your Telegram Bot Token
-
-
+API_ID = 26042863  
+API_HASH = "d4fabc00b0345cd3f0ccdc0c9b750f6e"  
+BOT_TOKEN = "7684406232:AAHOKnaKpHJQyChH1YmlsmzS86ZcU1vQByE"  
 MEGA_EMAIL = "logkithog442@fanclub.pm"
 MEGA_PASSWORD = "Hehe@2580"
-
 LOG_CHANNEL_ID = -1002320532990
-
 ADMIN_USER_IDS = [891959176, 987654321]
-
-# MongoDB Configuration
 MONGO_URI = "mongodb+srv://mongo920:LAZkTQrPibzZDArm@cluster0.wbyyv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "mydb"
 COLLECTION_NAME = "my"
